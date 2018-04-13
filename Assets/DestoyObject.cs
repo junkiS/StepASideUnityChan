708d85e029eb830a1cf3d46d00714375fae6cbc5
DESKTOP-R5OS0DA\junki <2pcolor@gmail.com>
2018-04-13 22:41:28 +0900

    レッスン６課題　Destroy関数でオブジェクトを消そう
    課題用コミット

---
+++ Assets/DestoyObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoyObject : MonoBehaviour
+{
+
+    private GameObject Unitychan;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.Unitychan = GameObject.Find("unitychan");
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Unitychan.transform.position.z  > this.transform.position.z)
+            {
+          
+                Destroy(this.gameObject,2f);
+            
+            }
+        }
+    }
+
+
+
+
+
